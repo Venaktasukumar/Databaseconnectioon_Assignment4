7378d9044b2f56d4aaf8f9a4e759cf75507782c0
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-31 14:10:54 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 import java.util.*;
 import java.util.logging.*;
-class Databaseconnectionn {
+class Databaseconnectioon{
     Logger l= Logger.getLogger("com.api.jar");
-    static Databaseconnectionn st;
+    static Databaseconnectioon st;
     String connection;
     String connect;
-    private Databaseconnectionn(String connection){
+    private Databaseconnectioon(String connection){
         this.connection=connection;
     }
-    public static Databaseconnectionn getInstance(String connection){
+    public static Databaseconnectioon getInstance(String connection){
         if(st==null){
-            st=new Databaseconnectionn(connection);
+            st=new Databaseconnectioon(connection);
         }
         return st;
     }
@@ -21,40 +21,31 @@ class Databaseconnectionn {
     public void closeconnection(){
         l.info("connection is closed");
     }
-    public static void main(String[] args){
-        Logger l= Logger.getLogger("com.api.jar");
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Logger l = Logger.getLogger("com.api.jar");
+        Scanner sc = new Scanner(System.in);
         l.info("Enter connection:");
-        String connection=sc.next();
-        Databaseconnectionn sv=Databaseconnectionn.getInstance(connection);
-        while(true) {
-            try {
-                l.info("1.New Connection");
-                l.info("2.Terminate connection");
-                l.info("3.Exit");
-                l.info("Enter your operation or choice");
-                int op = sc.nextInt();
-                if (op == 1) {
-                    l.info("Enter new connection");
-                    String s=sc.next();
-                    sv.connection(s);
-                }
-                else if (op == 2) {
-                    sv.closeconnection();
-                }
-                else if(op==3) {
-                    l.info("Exit");
-                    break;
-                }
-                else{
-                    l.info("invalid choice:choose correct option");
-                }
-            }
-            catch(Exception e) {
-                l.info("input mismatch");
-                sc.nextLine();
+        String connection = sc.next();
+        Databaseconnectioon sv = Databaseconnectioon.getInstance(connection);
+        while (true) {
+            l.info("1.New Connection");
+            l.info("2.Terminate connection");
+            l.info("3.Exit");
+            l.info("Enter your operation or choice");
+            int op = sc.nextInt();
+            if (op == 1) {
+                l.info("Enter new connection");
+                String s = sc.next();
+                sv.connection(s);
+            } else if (op == 2) {
+                sv.closeconnection();
+            } else if (op == 3) {
+                l.info("Exit");
+                break;
+            } else {
+                l.info("invalid choice:choose correct option");
             }
         }
         sc.close();
     }
-}
+    }
